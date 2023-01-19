43424af0e13a4fa49974708cd905d64e34062f8a
LuisFC <luisfernandezlf.70@gmail.com>
2023-01-19 12:07:44 +0100

    Segundo commit de HolaDAM Luis

--- HolaDAM/src/main/java/com/mycompany/holadam/HolaDAM.java
+++ HolaDAM/src/main/java/com/mycompany/holadam/HolaDAM.java
@@ -12,6 +12,6 @@ package com.mycompany.holadam;
 public class HolaDAM {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola a todos, soy Luis.");
     }
 }
